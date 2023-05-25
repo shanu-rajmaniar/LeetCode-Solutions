edff215e7d966906ebfdc4f44b652899684dc5cf
Shanu Rajmaniar <73025553+Shanu-Rajmaniar@users.noreply.github.com>
2023-05-25 18:26:42 +0530

    Time: 5 ms (27.98%), Space: 7.7 MB (54.25%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n = size(nums);
+        if(n <= 2) {
+            return *max_element(nums.begin(), nums.end());
+        }
+        int dp[n][2];
+        int ans1 = 0, ans2 = 0, ans3 = 0;
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i < n - 1; i++) {
+            dp[i][1] = nums[i] + dp[i - 1][0];
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
+        }
+        ans1 = max(dp[n - 2][0], dp[n - 2][1]);
+        memset(dp, 0, sizeof(dp));
+        for(int i = 1; i < n; i++) {
+            dp[i][1] = nums[i] + dp[i - 1][0];
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
+        }
+        ans2 = max(dp[n - 1][0], dp[n - 1][1]);
+        memset(dp, 0, sizeof(dp));
+        dp[0][1] = nums[0];
+        for(int i = 1; i < n; i++) {
+            dp[i][1] = nums[i] + dp[i - 1][0];
+            dp[i][0] = max(dp[i - 1][0], dp[i - 1][1]);
+        }
+        ans3 = max(dp[n - 1][0], dp[n - 1][1]);
+        return max({ans1, ans2, ans3});
+    }
+};
