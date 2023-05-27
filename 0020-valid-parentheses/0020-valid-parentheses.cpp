50fd6ca54133eee82eb2382a6fd2a3885a1c2564
Shanu Rajmaniar <73025553+Shanu-Rajmaniar@users.noreply.github.com>
2023-05-27 21:12:37 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (81.44%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<int> st;
+        for(int i = 0; i < size(s); i++) {
+            if(s[i] == '(' || s[i] == '{' || s[i] == '[') {
+                st.push(s[i]);
+            } else if(s[i] == ')') {
+                if(st.empty() || st.top() != '(') {
+                    return false;
+                }
+                st.pop();
+            } else if(s[i] == '}') {
+                if(st.empty() || st.top() != '{') {
+                    return false;
+                }
+                st.pop();
+            } else {
+                if(st.empty() || st.top() != '[') {
+                    return false;
+                }
+                st.pop();
+            }
+        }
+        return st.empty();
+    }
+};
