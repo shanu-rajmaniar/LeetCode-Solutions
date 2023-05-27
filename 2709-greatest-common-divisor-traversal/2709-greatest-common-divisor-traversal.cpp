2cf79d3ab2af9c86f02a5d3c5f1fb1ebc14a5955
Shanu Rajmaniar <73025553+Shanu-Rajmaniar@users.noreply.github.com>
2023-05-27 21:33:05 +0530

    Time: 470 ms (80.00%), Space: 135.5 MB (30.00%) - LeetHub

--- 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
+++ 2709-greatest-common-divisor-traversal/2709-greatest-common-divisor-traversal.cpp
@@ -1,58 +1,98 @@
-class UnionFind {
-    public:
-    vector<int> par, sz;
-    UnionFind(int n) : par(n), sz(n,1) {
-        iota(begin(par), end(par), 0);
+int m[100005];
+struct UnionFind {
+    int n, *parent, *rank;
+    UnionFind() {}
+    UnionFind(int a) {
+        n = a;
+        parent = new int[n + 1];
+        rank = new int[n + 1];
+        for(int i = 0; i < n; i++) {
+            parent[i] = i;
+            rank[i] = 1;
+        }
     }
     int find(int x) {
-        if(par[x] == x) return x;
-        return par[x] = find(par[x]);
+        if(x != parent[x]) {
+            return parent[x] = find(parent[x]);
+        }
+        return x;
     }
-    bool Union(int x, int y) {
-        int xp = find(x), yp = find(y);
-        if(xp == yp) return false;
-        if(sz[xp] > sz[yp]) par[yp] = par[xp], sz[xp] += sz[yp];
-        else par[xp] = par[yp], sz[yp] += sz[xp];
-        return true;
+    void merge(int x, int y) {
+        int xroot = find(x);
+        int yroot = find(y);
+        if(xroot != yroot) {
+            if(rank[xroot] >= rank[yroot]) {
+                parent[yroot] = xroot;
+                rank[xroot] += rank[yroot];
+            }
+            else {
+                parent[xroot] = yroot;
+                rank[yroot] += rank[xroot];
+            }
+        }  
+    }
+    int max_size() {
+        int ans = 1;
+        for(int i = 0; i < n; i++) {
+            ans = max(ans, rank[i]);
+        }
+        return ans;
     }
 };
+
 class Solution {
 public:
     bool canTraverseAllPairs(vector<int>& nums) {
-        int n = size(nums), ans = 1;
-        auto spf = sieve(*max_element(begin(nums), end(nums))+1);
-        UnionFind uf(n + 1);
-        unordered_map<int, int> mp;                      
-        for(int i = 0; i < n; i++) {
-            for(auto p : getPrimes(nums[i], spf)) {
-                if(mp.count(p)) uf.Union(i, mp[p]);
-                else mp[p] = i;
-            }  
-        }                    
-            
-        return *max_element(begin(uf.sz), end(uf.sz)) == nums.size();
-    }
-    
-    vector<int> sieve(int M) {
-        vector<int> spf(M+1);
-        iota(begin(spf), end(spf), 0);
-        for(int i = 2; i*i < M; i++) {
-            if(spf[i] == i) 
-                for(int j = i*i; j < M; j += i) {
-                    if(spf[j] == j)
-                        spf[j] = i;
-                }
-        }        
-        return spf;
-    }
-
-    vector<int> getPrimes(int x, vector<int>& spf) {
-        vector<int> primes;
-        while (x != 1) {
-            int p = spf[x];
-            primes.push_back(p);
-            while(x % p == 0) x /= p;
+        if(size(nums) == 1) return true;
+        if(count(begin(nums), end(nums), 1)) {
+            return false;
         }
-        return primes;
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int maxn = nums[n - 1] + 1;
+        for(int i = 0; i <= maxn; i++) {
+            m[i] = 0;
+        }
+        for(int i = 2; i < maxn; i++) {
+            if(m[i] == 0) {
+                m[i] = 1;
+                for(int j = 2; i * j <= maxn; j++) {
+                    m[i * j] = -1;
+                }
+            }
+        }
+        vector<vector<int>> v(maxn);
+        for(int i = 0; i < n; i++) {
+            int x = nums[i];
+            if(m[x] == 1) {
+                v[x].push_back(x);
+            }
+            else {
+                for(int j = 2; j <= sqrt(x) + 1; j++) {
+                    if(x % j == 0) {
+                        if(m[j] == 1 && j == x / j) {
+                            v[j].push_back(x);
+                        }
+                        else {
+                            if(m[j] == 1) v[j].push_back(x);
+                            if(m[x / j] == 1) v[x / j].push_back(x);
+                        }
+                    }
+                }
+            }
+        }
+        UnionFind uf(maxn);
+        for(int i = 0; i < maxn; i++) {
+            for(int j = 1; j < v[i].size(); j++) {
+                if(uf.find(v[i][0]) != uf.find(v[i][j])) {
+                    uf.merge(v[i][0], v[i][j]);
+                }
+            }
+        }
+        int ans = uf.max_size();
+        int uniq = 0;
+        set<int> st;
+        for(int i = 0; i < n; i++) st.insert(nums[i]);
+        return ans == (int)st.size();
     }
 };
